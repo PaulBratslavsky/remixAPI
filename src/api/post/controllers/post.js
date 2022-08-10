99fd2332d670b83dcd0b663ac26f2979130fa557
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2022-08-10 16:06:19 -0500

    added custom controller to enable get by slug

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ( {strapi}) => ({
+  async findBySlug(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const post = await strapi.entityService.findMany("api::post.post", query);
+    const sanitizedEntity = await this.sanitizeOutput(post);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
